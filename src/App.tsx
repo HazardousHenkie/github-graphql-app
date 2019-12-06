8bf247028dbcc7e39454cd544c97be022133bb08
Kyle <thekyle@hotmail.com>
2019-12-06 13:26:23 +0900

    added application error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { compose } from 'recompose'
 
 import { useSelector } from 'react-redux'
 
@@ -14,12 +13,13 @@ import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles'
 import { ThemeProvider } from '@material-ui/styles'
 
 import { WithAuthentication } from './components/authentication'
-import { withSnackbar } from './components/snackbar'
 
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
+import { ApolloLink } from 'apollo-link'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { onError } from 'apollo-link-error'
 
 let theme = createMuiTheme({
   palette: {
@@ -54,8 +54,23 @@ const App: React.FC = () => {
     }
   })
 
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) {
+      graphQLErrors.map(({ message, locations, path }) => {
+        console.log(
+          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+        )
+      })
+    }
+    if (networkError) {
+      console.log(`[Network error]: ${networkError}`)
+    }
+  })
+
+  const link = ApolloLink.from([errorLink, httpLink])
+
   const client = new ApolloClient({
-    link: httpLink,
+    link,
     cache: new InMemoryCache()
   })
 
@@ -74,4 +89,4 @@ const App: React.FC = () => {
     </ApolloProvider>
   )
 }
-export default compose(withSnackbar, WithAuthentication)(App)
+export default WithAuthentication(App)
