e407de35e4677bda006e41a853eb4f20dc01aea8
Kyle <thekyle@hotmail.com>
2019-12-08 21:05:21 +0900

    pulled function from jsx elements and added removestarfunctionality

--- src/components/repositories/repositoryItem.tsx
+++ src/components/repositories/repositoryItem.tsx
@@ -5,6 +5,7 @@ import './resporityItem.scss'
 import { useMutation } from '@apollo/react-hooks'
 import { starRepository, unstarRepository } from './mutations'
 import repositoryFragment from './fragments'
+import { DataProxy } from 'apollo-cache'
 
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
@@ -41,6 +42,87 @@ const RepositoryItem: React.FC<RepositoriesProps> = ({
   const [addStar] = useMutation(starRepository)
   const [removeStar] = useMutation(unstarRepository, { variables: { id } })
 
+  // lees wieruch ding nog eens ff door
+  // mss deze functions in apart bestand voor schoonheid
+
+  const getUpdatedStarData = (
+    proxy: DataProxy,
+    id: number,
+    viewerHasStarred: boolean
+  ) => {
+    const data: any = proxy.readFragment({
+      id: `Repository:${id}`,
+      fragment: repositoryFragment
+    })
+
+    let { totalCount } = data.stargazers
+    totalCount = viewerHasStarred ? totalCount + 1 : totalCount - 1
+    proxy.writeFragment({
+      id: `Repository:${id}`,
+      fragment: repositoryFragment,
+      data: {
+        ...data,
+        stargazers: { ...data.stargazers, totalCount }
+      }
+    })
+  }
+
+  const addStarWrapper = () =>
+    addStar({
+      variables: { id },
+      optimisticResponse: {
+        addStar: {
+          __typename: 'Mutation',
+          // check how this works more deeply but probably its getting the paramaters inside strarable (like the query)
+          starrable: {
+            __typename: 'Repository',
+            id,
+            viewerHasStarred: !viewerHasStarred
+
+            // can we put the whole thing here ? i think so
+          }
+        }
+      },
+      //i think since it's deep it need this update to tell us what to update or to prevent a full update//put this funciton in a seperate one outside of this one
+      update: (
+        proxy,
+        {
+          data: {
+            addStar: {
+              //same here get the data as mentioned above
+              starrable: { id, viewerHasStarred }
+            }
+          }
+        }
+      ) => getUpdatedStarData(proxy, id, viewerHasStarred)
+    })
+
+  const removeStarWrapper = () =>
+    removeStar({
+      variables: { id },
+      optimisticResponse: {
+        removeStar: {
+          __typename: 'Mutation',
+
+          starrable: {
+            __typename: 'Repository',
+            id,
+            viewerHasStarred: !viewerHasStarred
+          }
+        }
+      },
+      update: (
+        proxy,
+        {
+          data: {
+            removeStar: {
+              starrable: { id, viewerHasStarred }
+            }
+          }
+        }
+      ) => getUpdatedStarData(proxy, id, viewerHasStarred)
+    })
+
   return (
     <Card className="repository_card">
       <CardContent>
@@ -65,54 +147,13 @@ const RepositoryItem: React.FC<RepositoriesProps> = ({
           >
             Watchers {watchers}
           </Button>
+
           {!viewerHasStarred ? (
             <Button
               className="repository_card__button"
               variant="contained"
               color="secondary"
-              onClick={() =>
-                addStar({
-                  variables: { id },
-                  optimisticResponse: {
-                    __typename: 'Mutation',
-                    addStar: {
-                      __typename: 'Repository',
-                      id,
-                      viewerHasStarred: !viewerHasStarred
-                    }
-                  },
-                  update: (
-                    proxy,
-                    {
-                      data: {
-                        addStar: {
-                          starrable: { id, viewerHasStarred }
-                        }
-                      }
-                    }
-                  ) => {
-                    const data: any = proxy.readFragment({
-                      id: `Repository:${id}`,
-                      fragment: repositoryFragment
-                    })
-
-                    if (data) {
-                      let { totalCount } = data.stargazers
-                      totalCount = viewerHasStarred
-                        ? totalCount + 1
-                        : totalCount - 1
-                      proxy.writeFragment({
-                        id: `Repository:${id}`,
-                        fragment: repositoryFragment,
-                        data: {
-                          ...data,
-                          stargazers: { ...data.stargazers, totalCount }
-                        }
-                      })
-                    }
-                  }
-                })
-              }
+              onClick={() => addStarWrapper()}
               startIcon={<StarIcon />}
             >
               Stars {stargazers}
@@ -122,7 +163,7 @@ const RepositoryItem: React.FC<RepositoriesProps> = ({
               className="repository_card__button"
               variant="contained"
               color="primary"
-              onClick={() => removeStar()}
+              onClick={() => removeStarWrapper()}
               startIcon={<StarIcon />}
             >
               Stars {stargazers}
@@ -150,4 +191,4 @@ const RepositoryItem: React.FC<RepositoriesProps> = ({
   )
 }
 
-export default React.memo(RepositoryItem)
+export default RepositoryItem
